1490621f771dc02ad470b7eba4b47b363a336d52
UncleHandsome <lee2041412@gmail.com>
2016-07-19 12:20:28 +0800

    Add new theme

--- pelicanconf.py
+++ pelicanconf.py
@@ -12,6 +12,8 @@ TIMEZONE = 'Asia/Taipei'
 
 DEFAULT_LANG = 'en'
 
+THEME = "themes/tuxlite_tbs"
+
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
